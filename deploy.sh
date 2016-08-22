8e6f8749635fe0b1d37f8f2dacb0f0151f842e03
Todd Blackman <tdblackman@gmail.com>
2016-08-22 18:54:55 -0400

    More notes and assets

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,2 @@
 bundle exec middleman build
-aws s3 sync --region=us-east-1 --profile blackman build/ s3://windhamsite/ --delete
+aws s3 sync --region=us-east-1 --profile blackman build/ s3://windham.justwindham.club/ --delete
