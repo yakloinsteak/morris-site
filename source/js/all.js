2daa0aa43730762135151c371f53c32f0d15a76f
Todd Blackman <tdblackman@gmail.com>
2018-05-21 19:30:45 -0400

    progress

--- source/js/all.js
+++ source/js/all.js
@@ -1,9 +1,18 @@
 $('document').ready(function() {
   $('a[data-modal-src]').click(function() {
-    var src = $(this).data('modal-src');
-    var image = $('<img>').attr('src', src);
-    var title = $(this).html()
-    $('.modal-body').html(image);
+    var $this = $(this)
+    var src = $this.data('modal-src');
+
+    if (src.match(/html/)) {
+      $.get(src, function(content) {
+        $('.modal-body').html(content);
+      });
+    } else {
+      var content = $('<img>').attr('src', src);
+      $('.modal-body').html(content);
+    }
+    var title = $this.html()
+
     $('.modal-title').html(title)
     $('#myModal').modal();
   });
